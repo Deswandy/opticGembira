4154fddd2a98a5490cf04092aaa09244c360dd33
Deswandy <deswandywong23@gmail.com>
2025-06-01 01:54:56 +0700

    added shadcnui

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,10 +1,29 @@
 import { Link, Head } from '@inertiajs/react';
+import { IoGlassesOutline } from "react-icons/io5";
+import { z } from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod"
+import useForm from '@inertiajs/react';
 
 export default function Welcome(props) {
+
     return (
-        <>
+        //BIKIN LAYOUT UNTUK FULLSCREEN YA!
+        <div className=''>
             <Head title="Welcome" />
-            <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+            
+            <div className=''>
+                <div className='flex flex-row gap-2 items-center justify-center p-4 text-center bg-blue-700 md:justify-start md:px-8'>
+                    <IoGlassesOutline className='size-12 invert'/>
+                    <h1 className='font-extrabold text-xl text-white'>OPTIC GEMBIRA</h1>
+
+                </div>
+                
+                <div>
+
+                </div>
+            </div>
+
+            {/* <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
                 <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
                     {props.auth.user ? (
                         <Link
@@ -320,9 +339,9 @@ export default function Welcome(props) {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
-            <style>{`
+            {/* <style>{`
                 .bg-dots-darker {
                     background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
                 }
@@ -331,7 +350,7 @@ export default function Welcome(props) {
                         background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
                     }
                 }
-            `}</style>
-        </>
+            `}</style> */}
+        </div>
     );
 }
