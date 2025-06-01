1607a0cc7038d4663977349845bd587f0f0dfc0b
Gonda05 <pargonda.maryun@student.umn.ac.id>
2025-06-01 16:57:07 +0700

    selesai nyambungin erd

--- database/migrations/2025_05_30_050719_create_ms_kacamatas.php
+++ database/migrations/2025_05_30_050719_create_ms_kacamatas.php
@@ -13,13 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('ms_kacamatas', function (Blueprint $table) {
+      Schema::create('ms_kacamatas', function (Blueprint $table) {
             $table->id();
-            $table->int('ms_merks_id')->unsigned();
-            $table->int('ms_lacis_id')->unsigned();
-            $table->int('ms_kacamata_statuses_id')->unsigned();
+            $table->foreignId('ms_merks_id')->constrained('ms_merks');
+            $table->foreignId('ms_lacis_id')->constrained('ms_lacis');
+            $table->foreignId('ms_kacamata_statuses_id')->constrained('ms_kacamata_statuses');
             $table->string('tipe');
-            //TAMBAHKAN FIELD UNTUK FILE FOTO
+            $table->string('foto')->nullable(); // Field untuk file foto
             $table->string('bahan');
             $table->timestamps();
         });
