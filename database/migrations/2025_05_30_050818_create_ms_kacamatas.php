f88c222784746bbcec96088458158a6d35925146
Deswandy <deswandywong23@gmail.com>
2025-06-01 18:17:21 +0700

    redirect login

--- database/migrations/2025_05_30_050818_create_ms_kacamatas.php
+++ database/migrations/2025_05_30_050818_create_ms_kacamatas.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('ms_kacamatas', function (Blueprint $table) {
             $table->id();
-            $table->int('ms_merks_id')->unsigned();
-            $table->int('ms_lacis_id')->unsigned();
-            $table->int('ms_kacamata_statuses_id')->unsigned();
+            $table->integer('ms_merks_id')->unsigned();
+            $table->integer('ms_lacis_id')->unsigned();
+            $table->integer('ms_kacamata_statuses_id')->unsigned();
             $table->string('tipe');
             //TAMBAHKAN FIELD UNTUK FILE FOTO
             $table->string('bahan');
--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,12 +1,17 @@
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import { Link } from '@inertiajs/react';
+import { IoGlassesOutline } from "react-icons/io5"
 
 export default function Guest({ children }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
+                    <div className='flex flex-row gap-2 items-center justify-center p-4 text-center bg-blue-700 rounded-lg md:justify-start md:px-8'>
+                        <IoGlassesOutline className='size-12 invert' />
+                        <h1 className='font-extrabold text-xl text-white'>OPTIC GEMBIRA</h1>
+
+                    </div>
                 </Link>
             </div>
 
